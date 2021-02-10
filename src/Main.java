2a8c4d4ba3da25735f242b9149ef4e92010b4df8
Olivier <2032356@etu.cegepjonquiere.ca>
2021-02-10 16:40:24 -0500

    Version laboratoire 3 (Les messages d'erreurs et l'interface ont été amélioré ou ajouté.)

--- src/Main.java
+++ src/Main.java
@@ -12,52 +12,90 @@ public class Main {
         // Initialisation du Scanner
         Scanner sc = new Scanner(System.in);
 
-        // Variables
-        String choixCircuit;
-        String resistanceCircuit;
-
-        // Création d'un circuit
-        CircuitSerie circuitSerie = new CircuitSerie();
-        CircuitParallele circuitParallele = new CircuitParallele();
-
-        // Tableau des résisteurs
-        String tableauDesString[];
-
-        // Création d'un séparateur
-        final String SEPARATEUR = ",";
-
         do {
-            // Questionnairedo{
-            System.out.println("Voulez-vous entrer un circuit en (S)érie ou un (P)arallèle?");
+            // Variables diverses
+            String choixCircuit;
+            String resistanceCircuit;
+            String erreur = "          !!! Code ERREUR: Mauvaise entrée !!!"; // Message d'erreur pré-fait
+
+            // Variables ''Vérificateurs d'erreurs''
+            Boolean messageErreur = true;
+            Boolean messageErreur2 = false;
+
+            // Création d'un circuit
+            CircuitSerie circuitSerie = new CircuitSerie();
+            CircuitParallele circuitParallele = new CircuitParallele();
+
+            // Tableau des Srings résisteurs
+            String tableauDesString[];
+
+            // Création d'un séparateur
+            final String SEPARATEUR = ",";
+
+            // Questionnaire
+
+            System.out.println("\n" + "•-----------------------Calculatrice-----------------------•");
+            System.out.println("\n" + "Voulez-vous entrer un circuit en (S)érie ou un (P)arallèle?");
+            System.out.print("  >> ");
             choixCircuit = sc.next().toUpperCase();
 
-            System.out.println("Entrez des résistances séparées par des virgules");
-            resistanceCircuit = sc.next();
-
-            tableauDesString = resistanceCircuit.split(SEPARATEUR); //
-
             if (choixCircuit.charAt(0) == 'S') { // Il s'agit d'un circuit en Série
+                messageErreur = false; // Aucune erreur est détectée
+                System.out.println("Entrez des résistances séparées par des virgules.");
+                System.out.print("  >> ");
+                resistanceCircuit = sc.next();
+
+                tableauDesString = resistanceCircuit.split(SEPARATEUR); // Sépare la String entre les virgules et place les parties dans le tableau des Strings résitors
+
                 for (int i = 0; i < tableauDesString.length; i++) {
                     Resistor r = FabriqueResistor.fabriquerResistor(tableauDesString[i]);
-                    circuitSerie.ajouterResistor(r);
+                    if (r != null)
+                        circuitSerie.ajouterResistor(r);
+                    else
+                        messageErreur2 = true; // Erreur détectée
                 }
-                System.out.print("Entrez une tension en volts");
-                circuitSerie.mettreSousTension(sc.nextDouble());
-                System.out.println(" Resistance : " + circuitSerie.calculerResistance() + " Tension : " + circuitSerie.calculerTension() + " Courant : " + circuitSerie.calculerCourrant());
+                    if(messageErreur2 != true){
+                        System.out.println("Entrez une tension en volts.");
+                        System.out.print("  >> ");
+                        circuitSerie.mettreSousTension(sc.nextDouble());
+                        System.out.println("Résultats => Resistance : " + circuitSerie.calculerResistance() + "Ω Tension : " + circuitSerie.calculerTension() + "V Courant : " + circuitSerie.calculerCourrant() + "A");
+                    }
+                    else
+                        System.out.println(erreur); // Message d'erreur
+
             }
             if (choixCircuit.charAt(0) == 'P') { // Il s'agit d'un circuit en paralelle
+                messageErreur = false; // Aucune erreur est détectée
+                System.out.println("Entrez des résistances séparées par des virgules.");
+                System.out.print("  >> ");
+                resistanceCircuit = sc.next();
+
+                tableauDesString = resistanceCircuit.split(SEPARATEUR); // Sépare la String entre les virgules et les places dans un tableau
+
                 for (int i = 0; i < tableauDesString.length; i++) {
                     Resistor r = FabriqueResistor.fabriquerResistor(tableauDesString[i]);
-                    circuitParallele.ajouterResistor(r);
+                    if (r != null)
+                        circuitParallele.ajouterResistor(r);
+                    else
+                        messageErreur2 = true; // Erreur détectée
                 }
-                System.out.println("Entrez une tension en volts");
-                circuitParallele.mettreSousTension(sc.nextDouble());
-                System.out.println(" Resistance : " + circuitParallele.calculerResistance() + " Tension : " + circuitParallele.calculerTension() + " Courant : " + circuitParallele.calculerCourrant());
+                if (messageErreur2 != true){
+                    System.out.println("Entrez une tension en volts.");
+                    System.out.print("  >> ");
+                    circuitParallele.mettreSousTension(sc.nextDouble());
+                    System.out.println("Résultats => Resistance : " + circuitParallele.calculerResistance() + "Ω Tension : " + circuitParallele.calculerTension() + "V Courant : " + circuitParallele.calculerCourrant() + "A");
+                }
+                else
+                    System.out.println(erreur); // Message d'erreur
+            }
+            if(messageErreur == true){
+                System.out.println(erreur); // Message d'erreur
             }
         }while(true);
     }
 }
 
+// Main Laboratoire 2
 //do{
 //System.out.println("\n" + "•----------------Calculatrice----------------•");
 // System.out.println("\n" + "Veuiller entrer les couleurs d'une résistance.");
